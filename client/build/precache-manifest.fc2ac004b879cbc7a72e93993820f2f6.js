60f459ed5cb111277667e6e72245a11a7805a345
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2023-12-31 22:01:59 -0800

    fixed server.js

---
+++ client/build/precache-manifest.fc2ac004b879cbc7a72e93993820f2f6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67990bcbfdb88e120a28f71e5be97a5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c65b024a3c284211c41",
+    "url": "/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "f49537b3839c6c316644",
+    "url": "/static/css/main.70048d4c.chunk.css"
+  },
+  {
+    "revision": "2c65b024a3c284211c41",
+    "url": "/static/js/2.9518a342.chunk.js"
+  },
+  {
+    "revision": "c3ba569dbd64cd6222f6cc1b5e786cf2",
+    "url": "/static/js/2.9518a342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49537b3839c6c316644",
+    "url": "/static/js/main.7aa8f1f0.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "49cb9dd041cbfbe29c15b9fb088a8a99",
+    "url": "/static/media/heroone.49cb9dd0.jpg"
+  },
+  {
+    "revision": "3ca0db3d8c6c73f896e6660caa55a8d0",
+    "url": "/static/media/herothree.3ca0db3d.jpg"
+  },
+  {
+    "revision": "9689fd14120c70ce7fd96581e0673fbf",
+    "url": "/static/media/herotwo.9689fd14.jpg"
+  },
+  {
+    "revision": "51fd416994c34246532686edb552e109",
+    "url": "/static/media/standardcarport.51fd4169.jpg"
+  },
+  {
+    "revision": "fa773efc5e9ea7d9ceb0688f39e31bd2",
+    "url": "/static/media/video.fa773efc.mp4"
+  }
+]);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-require('dotenv').config({ path: "./.env" });
+require('dotenv').config({ path: "./config.env" });
 dotenv.config();
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
@@ -15,7 +15,7 @@ const signedRoute = require('./routes/signedRoute');
 mongoose.set('strictQuery', true);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, { useUnifiedTopology : true, useNewUrlParser : true } )
   .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
@@ -32,10 +32,10 @@ app.use('/api/signedupusers', signedRoute);
 __dirname = path.resolve();
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', function (req, res) {
-      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   })
 } else {
   app.get("/", function (req, res) {
