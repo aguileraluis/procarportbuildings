0d9b79d92b83e8d53172b5adfd543b2caec268cf
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-02-22 21:28:08 -0800

    fixed

--- routes/stripe.js
+++ routes/stripe.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const KEY = process.env.STRIPE_KEY
 const stripe = require("stripe")('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');
 
-router.post("/payment", (req, res) => {
+router.post("/api/payment", (req, res) => {
   stripe.charges.create(
     {
       source: req.body.tokenId,
